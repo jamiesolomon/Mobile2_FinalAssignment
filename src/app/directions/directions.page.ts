04e5bddba9ae0eca51259470e516fb41cf370dbe
Emma Junger <emmajeane.junger@gmail.com>
2022-04-20 21:56:50 -0400

    finished directions

--- src/app/directions/directions.page.ts
+++ src/app/directions/directions.page.ts
@@ -10,9 +10,13 @@ declare let google;
 })
 export class DirectionsPage implements AfterViewInit {
   @ViewChild('mapContainer', { static: false }) mapElement;
+  directionsService = new google.maps.DirectionsService();
+  directionsRenderer = new google.maps.DirectionsRenderer();
   public map;
   public address;
   public location;
+  public source = '';
+  public destination = '';
   public opts = {
     center: { lat: 43.653225, lng: -79.383186 },
     zoom: 8,
@@ -30,15 +34,42 @@ export class DirectionsPage implements AfterViewInit {
     this.geocodeLatLng(this.opts.center);
   }
 
+  loadDirections() {
+    this.map = new google.maps.Map(this.mapElement.nativeElement, this.opts);
+    this.directionsRenderer.setMap(this.map);
+    this.calcRoute();
+  }
+
+  calcRoute() {
+    this.directionsService.route(
+      {
+        origin: {
+          query: this.source,
+        },
+        destination: {
+          query: this.destination,
+        },
+        travelMode: google.maps.TravelMode.DRIVING,
+      },
+      (res, status) => {
+        if (status === 'OK') {
+          this.directionsRenderer.setDirections(res);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      }
+    );
+  }
+
   geocodeLatLng(currentPosition) {
     const geocoder = new google.maps.Geocoder();
     geocoder
       .geocode({ location: currentPosition })
-      .then((response) => {
-        if (response.results[0]) {
-          this.address = response.results[0].formatted_address;
-          if (response.results[0].address_components.length > 0) {
-            response.results[0].address_components.forEach((element) => {
+      .then((res) => {
+        if (res.results[0]) {
+          this.address = res.results[0].formatted_address;
+          if (res.results[0].address_components.length > 0) {
+            res.results[0].address_components.forEach((element) => {
               if (element.types.indexOf('locality') !== -1) {
                 this.location = element.short_name;
               }
@@ -56,13 +87,21 @@ export class DirectionsPage implements AfterViewInit {
       .catch((e) => window.alert('Geocoder failed due to: ' + e));
   }
 
+  onPress(event: any) {
+    if (event) {
+      this.loadDirections();
+    } else {
+      this.loadMap();
+    }
+  }
+
   ngAfterViewInit(): void {
     this.geo
       .getCurrentPosition()
       .then((res) => {
         this.opts.center.lat = res.coords.latitude;
         this.opts.center.lng = res.coords.longitude;
-        this.loadMap();
+        this.onPress(event);
       })
       .catch((err) => {
         console.log('location error', err);
